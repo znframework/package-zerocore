8024ffa252d83a3012eff2b02faea0174e8070dd
Ozan UYKUN <znframework@gmail.com>
2018-04-24 16:13:58 +0300

    The use of the subdirectory controller has been removed.

--- Structure.php
+++ Structure.php
@@ -21,12 +21,10 @@ class Structure
     public static function data($requestUri = NULL)
     {
         $namespace    = PROJECT_CONTROLLER_NAMESPACE;
-        $page         = '';
-        $openFunction = Config::get('Routing', 'openFunction') ?: 'main';
-        $function     = $openFunction ?: 'main';
+        $openFunction = $function = Config::get('Routing', 'openFunction') ?: 'main';
         $parameters   = [];
-        $segments     = '';
         $isFile       = '';
+        $page         = '';
         $url          = explode('?', $requestUri ?? In::requestURI());
         $segments     = explode('/', $url[0]);
 
@@ -40,30 +38,6 @@ class Structure
             if( ! is_file($isFile) )
             {
                 $isFile = EXTERNAL_CONTROLLERS_DIR . $suffixExtension;
-            }
-
-            if( ! is_file($isFile) )
-            {
-                $if        = '';
-                $nsegments = $segments;
-
-                for( $i = 0; $i < count($segments); $i++ )
-                {
-                    $if    .= $segments[$i].'/';
-                    $ifTrim = rtrim($if, '/');
-                    $isF    = CONTROLLERS_DIR . Base::suffix($ifTrim , '.php');
-
-                    if( is_file($isF) )
-                    {
-                        $page     = Datatype::divide($ifTrim, '/', -1);
-                        $isFile   = $isF;
-                        $segments = $nsegments;
-
-                        break;
-                    }
-
-                    array_shift($nsegments);
-                }
             }
 
             unset($segments[0]);
